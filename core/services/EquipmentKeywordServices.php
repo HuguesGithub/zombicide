f563859d924f89c004fd28d6a64193bb1cd03fd8
Hugues Joneaux <jhugues@jhugues.fr>
2018-06-30 17:46:29 +0200

    SonarCloud Fixes

--- core/services/EquipmentKeywordServices.php
+++ core/services/EquipmentKeywordServices.php
@@ -1,27 +1,32 @@
 <?php
-if (!defined('ABSPATH') ) { die('Forbidden' ); }
+if (!defined('ABSPATH')) {
+  die('Forbidden');
+}
 /**
  * Classe EquipmentKeywordServices
  * @author Hugues.
  * @version 1.0.00
  * @since 1.0.00
  */
-class EquipmentKeywordServices extends LocalServices {
+class EquipmentKeywordServices extends LocalServices
+{
   /**
    * L'objet Dao pour faire les requêtes
    * @var EquipmentKeywordDaoImpl $Dao
    */
   protected $Dao;
   
-  public function __construct() {
+  public function __construct()
+  {
     $this->Dao = new EquipmentKeywordDaoImpl();
     parent::__construct();
   }
 
-  private function buildFilters($arrFilters) {
+  private function buildFilters($arrFilters)
+  {
     $arrParams = array();
-    $arrParams[] = (isset($arrFilters['equipmentCardId']) ? $arrFilters['equipmentCardId'] : '%' );
-    $arrParams[] = (isset($arrFilters['keywordId']) ? $arrFilters['keywordId'] : '%' );
+    $arrParams[] = (isset($arrFilters['equipmentCardId']) ? $arrFilters['equipmentCardId'] : '%');
+    $arrParams[] = (isset($arrFilters['keywordId']) ? $arrFilters['keywordId'] : '%');
     return $arrParams;
   }
   /**
@@ -32,11 +37,11 @@ class EquipmentKeywordServices extends LocalServices {
    * @param string $order
    * @return array
    */
-  public function getEquipmentKeywordsWithFilters($file, $line, $arrFilters=array(), $orderby='id', $order='asc') {
+  public function getEquipmentKeywordsWithFilters($file, $line, $arrFilters=array(), $orderby='id', $order='asc')
+  {
     $arrParams = $this->buildOrderAndLimit($orderby, $order);
     $arrParams[SQL_PARAMS_WHERE] = $this->buildFilters($arrFilters);
     return $this->Dao->selectEntriesWithFilters($file, $line, $arrParams);
   }
   
 }
-?>
